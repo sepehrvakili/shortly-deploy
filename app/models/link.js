278ebd47ff4c33d255b25ed06da19ae1e4a68791
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 12:06:50 -0700

    complete promisifying routes

--- app/models/link.js
+++ app/models/link.js
@@ -2,22 +2,6 @@ var db = require('../config');
 var crypto = require('crypto');
 var mongoose = require('mongoose');
 
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-  // initialize: function() {
-  //   this.on('creating', function(model, attrs, options) {
-  //     var shasum = crypto.createHash('sha1');
-  //     shasum.update(model.get('url'));
-  //     model.set('code', shasum.digest('hex').slice(0, 5));
-  //   });
-  // }
-// });
-
-
 var linkSchema = new mongoose.Schema({
   id: Number,
   url: String,
@@ -42,5 +26,3 @@ linkSchema.pre('save', function(next) {
 var Link = mongoose.model('Link', linkSchema);
 
 module.exports = Link;
-
-
--- app/models/user.js
+++ app/models/user.js
@@ -3,26 +3,6 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function() {
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-  // hashPassword: function() {
-  //   var cipher = Promise.promisify(bcrypt.hash);
-  //   return cipher(this.get('password'), null, null).bind(this)
-  //     .then(function(hash) {
-  //       this.set('password', hash);
-  //     });
-  // }
-// });
-
 var userSchema = new mongoose.Schema({
   id: Number,
   username: String,
@@ -33,23 +13,11 @@ var userSchema = new mongoose.Schema({
   }
 );
 
-
-// userSchema.post('save', function(attemptedPassword, callback) {
-//   console.log('start user COMPARED');
-//   console.log('PASSWERD', this.get('password'));
-//   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//     callback(isMatch);
-//   });
-// });
-
 userSchema.methods.comparePassword = function(attemptedPassword, callback) {
   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
     callback(isMatch);
   });
 };
-
-// post - save is giving us issues with saving items into our db
-// setting models' properties after saving isn't storing in the db
 
 userSchema.pre('save', function(next) {
   var cipher = Promise.promisify(bcrypt.hash);
@@ -59,14 +27,6 @@ userSchema.pre('save', function(next) {
       next();
     });
 });
-
-// userSchema.methods.hashPassword = function(password) {
-//   var cipher = Promise.promisify(bcrypt.hash);
-//   return cipher(password, null, null).bind(this)
-//     .then(function(hash) {
-//       return hash;
-//     });
-// };
 
 var User = mongoose.model('User', userSchema);
 
